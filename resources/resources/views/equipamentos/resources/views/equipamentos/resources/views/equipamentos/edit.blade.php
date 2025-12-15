f08ada6743c3d7095ec5910b9d18124c12f7022e
Josefilipicruzdemiraes <108238809+Josefilipicruzdemiraes@users.noreply.github.com>
2025-12-15 15:51:11 -0300

    Formulário de edição de equipamentos
    Added a form for editing equipment details, including fields for name, type, and power.

--- resources/resources/views/equipamentos/resources/views/equipamentos/resources/views/equipamentos/edit.blade.php
+++ resources/resources/views/equipamentos/resources/views/equipamentos/resources/views/equipamentos/edit.blade.php
@@ -1,2 +1,20 @@
 <h1>Editar Equipamento</h1>
-<p>Tela de edição (UPDATE)</p>
+
+<form method="POST" action="/equipamentos/{{ $equipamento->id }}">
+    @csrf
+    @method('PUT')
+
+    <label>Nome:</label><br>
+    <input type="text" name="nome" value="{{ $equipamento->nome }}"><br><br>
+
+    <label>Tipo:</label><br>
+    <input type="text" name="tipo" value="{{ $equipamento->tipo }}"><br><br>
+
+    <label>Potência:</label><br>
+    <input type="text" name="potencia" value="{{ $equipamento->potencia }}"><br><br>
+
+    <button type="submit">Atualizar</button>
+</form>
+
+<br>
+<a href="/equipamentos">Voltar</a>
