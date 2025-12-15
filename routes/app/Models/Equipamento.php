5850d7da02e582927f41b01da90674542a048919
Josefilipicruzdemiraes <108238809+Josefilipicruzdemiraes@users.noreply.github.com>
2025-12-15 15:40:48 -0300

    Controller utilizando o model Equipamento

--- routes/app/Models/Equipamento.php
+++ routes/app/Models/Equipamento.php
@@ -1,10 +1,54 @@
 <?php
 
-namespace App\Models;
+namespace App\Http\Controllers;
 
-use Illuminate\Database\Eloquent\Model;
+use App\Models\Equipamento;
+use Illuminate\Http\Request;
 
-class Equipamento extends Model
+class EquipamentoController extends Controller
 {
-    protected $fillable = ['nome', 'tipo', 'potencia'];
+    // LISTAR (READ)
+    public function index()
+    {
+        $equipamentos = Equipamento::all();
+        return view('equipamentos.index', compact('equipamentos'));
+    }
+
+    // FORMULÁRIO DE CADASTRO
+    public function create()
+    {
+        return view('equipamentos.create');
+    }
+
+    // SALVAR (CREATE)
+    public function store(Request $request)
+    {
+        Equipamento::create($request->all());
+        return redirect()->route('equipamentos.index');
+    }
+
+    // FORMULÁRIO DE EDIÇÃO
+    public function edit($id)
+    {
+        $equipamento = Equipamento::findOrFail($id);
+        return view('equipamentos.edit', compact('equipamento'));
+    }
+
+    // ATUALIZAR (UPDATE)
+    public function update(Request $request, $id)
+    {
+        $equipamento = Equipamento::findOrFail($id);
+        $equipamento->update($request->all());
+
+        return redirect()->route('equipamentos.index');
+    }
+
+    // EXCLUIR (DELETE)
+    public function destroy($id)
+    {
+        $equipamento = Equipamento::findOrFail($id);
+        $equipamento->delete();
+
+        return redirect()->route('equipamentos.index');
+    }
 }
