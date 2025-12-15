3964b1e5d209402f8f78aec46c69460f0775d2d5
Josefilipicruzdemiraes <108238809+Josefilipicruzdemiraes@users.noreply.github.com>
2025-12-15 15:48:51 -0300

    Tela de listagem de equipamentos

--- resources/resources/views/equipamentos/index.blade.php
+++ resources/resources/views/equipamentos/index.blade.php
@@ -1,2 +1,20 @@
 <h1>Lista de Equipamentos</h1>
-<p>Tela de listagem (READ)</p>
+
+<a href="/equipamentos/create">Novo Equipamento</a>
+
+<ul>
+@foreach ($equipamentos as $equipamento)
+    <li>
+        <strong>{{ $equipamento->nome }}</strong>
+        ({{ $equipamento->tipo }} - {{ $equipamento->potencia }})
+
+        | <a href="/equipamentos/{{ $equipamento->id }}/edit">Editar</a>
+
+        <form action="/equipamentos/{{ $equipamento->id }}" method="POST" style="display:inline;">
+            @csrf
+            @method('DELETE')
+            <button type="submit">Excluir</button>
+        </form>
+    </li>
+@endforeach
+</ul>
